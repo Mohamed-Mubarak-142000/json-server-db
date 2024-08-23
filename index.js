39b38c145ffedeaf4073aa2f9a76f3729af04ff4
Mohamed Mubarak omar <mohamedmubarak142000m@gmail.com>
2024-08-23 04:26:47 +0300

    finshed

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const router = jsonServer.router("db.json"); // Path to your db.json file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
 server.use(router);
-server.listen(3000, () => {
+
+server.listen(5000, () => {
   console.log("JSON Server is running");
 });
